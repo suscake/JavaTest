56f35aa5cd46019688faa3e01b64ff45e8e37fda
Your Name <you@example.com>
2017-06-20 21:12:52 +0800

    new files added

---
+++ src/main/java/JavaMavenGitTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by geralt on 17-6-20.
+ */
+
+import org.joda.time.LocalTime;
+
+public class JavaMavenGitTest {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+    }
+
+}
